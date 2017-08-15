76c69b561d5dc26f0d36f1c8cc404b27bc70d1be
Ben J. Ward <ward9250@gmail.com>
2017-08-15 16:06:35 +0100

    try using channel in a for loop

--- src/bioseq/site_counting/count_sites_bitpar.jl
+++ src/bioseq/site_counting/count_sites_bitpar.jl
@@ -15,38 +15,21 @@
 function bitpar_counter(::Type{S}, a::BioSequence{A}, b::BioSequence{A}) where {S<:Site,A}
     bits_channel = aligned_bits(a, b)
     counts = bp_start_counter(S, A)
-    println("Do first block.")
     block = take!(bits_channel)
-    #println("Block:")
-    #println(block)
     x, y = bit_chunks(block)
     counts = bp_update_counter(counts, bp_chunk_count(S, A, x, y))
-    #println("First counts: ", counts)
-    #println("Was it an aligned head block: ", ishead(block))
-    #println("Correct for emptyspace: ", bp_correct_emptyspace(S, A))
     if ishead(block) && bp_correct_emptyspace(S, A)
         nempty = div(64, bitsof(A)) - div(remaining(block), bitsof(A))
-        #println("Empty: ", nempty)
         counts = bp_emptyspace_correction(nempty, counts)
-        #println("Corrected counts: ", counts)
     end
-    println("Done first block.")
-    println(isopen(bits_channel))
-    while isopen(bits_channel)
-        #println("In the body code block.")
-        block = take!(bits_channel)
-        #println("Block: ")
-        #println(block)
+    for block in bits_channel
+        block = next(bits_channel)
         x, y = bit_chunks(block)
         counts = bp_update_counter(counts, bp_chunk_count(S, A, x, y))
-        #println("Counts: ", counts)
     end
-    if istail(block)
-        #println("In the tail code block.")
-        if bp_correct_emptyspace(S, A)
-            nempty = div(64, bitsof(A)) - div(remaining(block), bitsof(A))
-            counts = bp_emptyspace_correction(nempty, counts)
-        end
+    if istail(block) && bp_correct_emptyspace(S, A)
+        nempty = div(64, bitsof(A)) - div(remaining(block), bitsof(A))
+        counts = bp_emptyspace_correction(nempty, counts)
     end
     return counts
 end
