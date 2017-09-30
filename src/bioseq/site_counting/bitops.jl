c1fc161b3ad3838d16cc4e0be399428092fdd13c
Ben J. Ward <ward9250@gmail.com>
2017-09-30 14:37:26 +0100

    Seperated bitops into own source file & added position counter

---
+++ src/bioseq/site_counting/bitops.jl
@@ -0,0 +1,68 @@
+# Bitparallel counting
+# --------------------
+
+# Now we overload some internal methods for each S <: Site.
+# So as different types of site can be counted in bit-parallel
+# manner.
+
+for A in (DNAAlphabet, RNAAlphabet)
+    @eval begin
+
+        # Gaps
+        @inline function bp_chunk_count(::Type{Gap}, ::Type{$A{4}}, x::UInt64)
+            return count_zero_nibbles(x)
+        end
+
+        @inline function bp_chunk_count(::Type{Gap}, ::Type{$A{4}}, a::UInt64, b::UInt64)
+            # Count the gaps in a, count the gaps in b, subtract the number of shared gaps.
+            return count_zero_nibbles(a) + count_zero_nibbles(b) - count_zero_nibbles(a | b)
+        end
+
+        # Certain
+        @inline function bp_chunk_count(::Type{Certain}, ::Type{$A{4}}, x::UInt64)
+            x = enumerate_nibbles(x)
+            x = x ⊻ 0x1111111111111111
+            return count_zero_nibbles(x)
+        end
+
+        @inline function bp_chunk_count(::Type{Certain}, ::Type{$A{4}}, a::UInt64, b::UInt64)
+            x = enumerate_nibbles(a) ⊻ 0x1111111111111111
+            y = enumerate_nibbles(b) ⊻ 0x1111111111111111
+            return count_zero_nibbles(x | y)
+        end
+
+        # Ambiguous
+        @inline function bp_chunk_count(::Type{Ambiguous}, ::Type{$A{4}}, x::UInt64)
+            return count_nonzero_nibbles(enumerate_nibbles(x) & 0xEEEEEEEEEEEEEEEE)
+        end
+
+        @inline function bp_chunk_count(::Type{Ambiguous}, ::Type{$A{4}}, a::UInt64, b::UInt64)
+            return count_nonzero_nibbles((enumerate_nibbles(a) | enumerate_nibbles(b)) & 0xEEEEEEEEEEEEEEEE)
+        end
+
+        # Match
+        @inline function bp_chunk_count(::Type{Match}, ::Type{$A{4}}, a::UInt64, b::UInt64)
+            return count_zero_nibbles(a ⊻ b)
+        end
+
+        @inline function bp_chunk_count(::Type{Match}, ::Type{$A{2}}, a::UInt64, b::UInt64)
+            return count_zero_bitpairs(a ⊻ b)
+        end
+
+        # Mismatch
+        @inline function bp_chunk_count(::Type{Mismatch}, ::Type{$A{4}}, a::UInt64, b::UInt64)
+            return count_nonzero_nibbles(a ⊻ b)
+        end
+
+        @inline function bp_chunk_count(::Type{Mismatch}, ::Type{$A{2}}, a::UInt64, b::UInt64)
+            return count_nonzero_bitpairs(a ⊻ b)
+        end
+    end
+end
+
+bp_correct_emptyspace(::Type{<:Site}, ::Type{<:Alphabet}) = false
+for s in (Match, Gap)
+    @eval bp_correct_emptyspace(::Type{$s}, ::Type{A}) where {A<:NucAlphs} = true
+end
+
+bp_emptyspace_correction(nempty::Int, count::Int) = count - nempty
---
+++ src/bioseq/site_counting/position_counter.jl
@@ -0,0 +1,36 @@
+
+struct PositionCounter{P<:Position} <: BitAlignedDoState
+    count::Int
+end
+
+function head_update_state(state::PositionCounter{P}, ::Type{A},
+                                x::UInt64, y::UInt64, k::Integer,
+                                m::UInt64) where {P <: Position, A <: Alphabet}
+
+    c = bp_chunk_count(P, A, x & m, y & m)
+    c += state.count
+
+    if bp_correct_emptyspace(P, A)
+        #println("Correcting for emptyspace...")
+        nempty = elems_per_chunk(A) - elems_per_x(k, A)
+        #println("nempty: ", nempty)
+        c -= nempty
+        #println("counts: ", counts)
+    end
+
+    return PositionCounter{P}(c)
+end
+
+function update_state(state::PositionCounter{P}, ::Type{A}, x::UInt64,
+                      y::UInt64) where {P <: Position, A <: Alphabet}
+
+    c = bp_chunk_count(P, A, x, y)
+    c += state.count
+end
+
+function tail_update_state(state::PositionCounter{P}, ::Type{A},
+                           x::UInt64, y::UInt64, k::Integer,
+                           m::UInt64) where {P <: Position, A <: Alphabet}
+
+    return head_update_state(state, A, x, y, k, m)
+end
