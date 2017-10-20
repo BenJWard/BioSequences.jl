66cf08e9169f711631207c49b09467f5890a6238
Ben J. Ward <ward9250@gmail.com>
2017-10-20 14:26:05 +0100

    Added the Codon{T} typealias

--- src/kmer.jl
+++ src/kmer.jl
@@ -33,8 +33,9 @@ primitive type Kmer{T<:NucleicAcid, K} <: Sequence 64 end
 
 const DNAKmer{K} = Kmer{DNA, K}
 const RNAKmer{K} = Kmer{RNA, K}
-const DNACodon = DNAKmer{3}
-const RNACodon = RNAKmer{3}
+const Codon{T} = Kmer{T, 3}
+const DNACodon = Codon{DNA}
+const RNACodon = Codon{RNA}
 
 function Kmer(nts::T...) where {T<:NucleicAcid}
     return make_kmer(nts)
