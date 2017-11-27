f40736a101b833857277e338f20f7aad4464671a
Ben J. Ward <ward9250@gmail.com>
2017-11-27 12:21:58 +0000

    alphabet_t kmers fix

--- src/kmer.jl
+++ src/kmer.jl
@@ -121,8 +121,8 @@ Base.convert(::Type{S}, seq::Kmer) where {S<:AbstractString} = convert(S, [Char(
 
 alphabet(::Type{DNAKmer{k}}) where {k} = (DNA_A, DNA_C, DNA_G, DNA_T)
 alphabet(::Type{RNAKmer{k}}) where {k} = (RNA_A, RNA_C, RNA_G, RNA_U)
-alphabet_t(::Type{Kmer{DNA, K}}) where K = DNAAlphabet{2} 
-alphabet_t(::Type{Kmer{RNA, K}}) where K = RNAAlphabet{2}
+alphabet_t(::Type{T}) where T <: Kmer{DNA} = DNAAlphabet{2} 
+alphabet_t(::Type{T}) where T <: Kmer{RNA} = RNAAlphabet{2}
 
 Base.hash(x::Kmer, h::UInt) = hash(UInt64(x), h)
 
