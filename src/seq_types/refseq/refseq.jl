60ddb2db1459f30fa993f1b3c099bde2dde3a665
Ben J. Ward <ward9250@gmail.com>
2018-02-05 22:39:06 +0000

    comment out debug lines

--- src/seq_types/refseq/refseq.jl
+++ src/seq_types/refseq/refseq.jl
@@ -96,7 +96,7 @@ end
 
 # Create ReferenceSequence object from the ascii-encoded `data`
 function encode(src::Vector{UInt8}, from::Integer, len::Integer)
-    println("src: ", src)
+    #println("src: ", src)
     data = zeros(UInt64, cld(len, 32))
     nmask = falses(len)
     #next = bitindex(1, 2)
@@ -105,23 +105,23 @@ function encode(src::Vector{UInt8}, from::Integer, len::Integer)
     #stop = bitindex(len + 1, 2)
     #stop = BitIndex{2, UInt64}(len + 1)
     stop = bitindex(Val{2}(), UInt64, len + 1)
-    println("next: ", next)
-    println("stop: ", stop)
+    #println("next: ", next)
+    #println("stop: ", stop)
     i = from
     while next < stop
         x = UInt64(0)
         j = index(next)
-        println("x: ", x)
-        println("j: ", j)
+        #println("x: ", x)
+        #println("j: ", j)
         while index(next) == j && next < stop
             # FIXME: Hotspot
             char = convert(Char, src[i])
             nt = convert(DNA, char)
-            println("char: ", char)
-            println("nt: ", nt)
-            println("!isambiguous: ", !isambiguous(nt))
+            #println("char: ", char)
+            #println("nt: ", nt)
+            #println("!isambiguous: ", !isambiguous(nt))
             if !isambiguous(nt)
-                println("Encoded nt: ", hex(encode(DNAAlphabet{2}, nt) << offset(next)))
+                #println("Encoded nt: ", hex(encode(DNAAlphabet{2}, nt) << offset(next)))
                 x |= UInt64(encode(DNAAlphabet{2}, nt)) << offset(next)
             elseif nt == DNA_N
                 nmask[i] = true
@@ -130,7 +130,7 @@ function encode(src::Vector{UInt8}, from::Integer, len::Integer)
             end
             i += 1
             next += 2
-            println("i and next: ", i, ", ", next)
+            #println("i and next: ", i, ", ", next)
         end
         data[j] = x
     end
