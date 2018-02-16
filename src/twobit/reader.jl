1e3db5359797b4eb91f71cf2629bd6ee0e638569
Ben J. Ward <ward9250@gmail.com>
2018-02-16 11:34:21 +0000

    improved read twobit data debug line

--- src/twobit/reader.jl
+++ src/twobit/reader.jl
@@ -159,7 +159,7 @@ function Base.read!(reader::Reader, record::Record)
         resize!(record.packeddna, datalen)
     end
     unsafe_read(reader.input, pointer(record.packeddna), datalen)
-    println("READ TWOBIT DATA: ", record.packeddna)
+    println("READ TWOBIT DATA: ", (bits(x) for x in record.packeddna)...)
     record.filled = true
     return record
 end
