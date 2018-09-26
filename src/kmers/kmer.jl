893acea382c629fe484902a456f29c7000d4ca60
Ben J. Ward <ward9250@gmail.com>
2018-09-26 22:23:51 +0100

    Move comments to shortsequence.jl

--- src/kmers/kmer.jl
+++ src/kmers/kmer.jl
@@ -1,33 +1,5 @@
 # K-mer
 # =====
-#
-# Compact k-mer sequence type.
-#
-# A Kmer is a sequence ≤ 32nt, without any 'N's, packed in a single 64 bit
-# value.  While `GeneralSequence` is an efficient general-purpose sequence
-# representation, Kmer is useful for applications like assembly, k-mer counting,
-# k-mer based quantification in RNA-Seq, etc that rely on manipulating many
-# short sequences as efficiently in space and time as possible.
-#
-# This file is a part of BioJulia.
-# License is MIT: https://github.com/BioJulia/BioSequences.jl/blob/master/LICENSE.md
-
-# Representation
-# --------------
-#
-# Four kinds of nucleotides are encoded as follows:
-#
-#   nucleotide | binary
-#   ---------- | ------
-#       A      |   00
-#       C      |   01
-#       G      |   10
-#     T / U    |   11
-#
-# NucleicAcids are filled from MSBs to LSBs and right-aligned so that all k-mers
-# are lexicographically ordered. For example, the memory layout of "TACG" is:
-#   64-bit: 0b 00 00 … 00 11 00 01 10
-#    4-mer:                T  A  C  G
 
 primitive type Kmer{T <: NucleicAcid, K} <: ShortSequence{64} 64 end
 
@@ -35,7 +7,6 @@ const DNAKmer{K} = Kmer{DNA, K}
 const RNAKmer{K} = Kmer{RNA, K}
 const DNACodon = DNAKmer{3}
 const RNACodon = RNAKmer{3}
-
 
 function Kmer(nts::T...) where {T<:NucleicAcid}
     return make_kmer(nts)
--- src/shortsequence/shortsequence.jl
+++ src/shortsequence/shortsequence.jl
@@ -1,3 +1,34 @@
+# ShortSequence
+# =============
+#
+# A compact short sequence type.
+#
+# Sometimes applications do not use long sequences, but short ones.
+# Such applications include tools that analyse and assemble kmers generated from
+# reads, or tools that analyse codons: short 3bp long sequences that correspond to
+# amino acids when a gene is translated into its protein product.
+# It is conveinient to represent such sequences using single 'words'.
+#
+# This file is a part of BioJulia.
+# License is MIT: https://github.com/BioJulia/BioSequences.jl/blob/master/LICENSE.md
+
+# Representation
+# --------------
+#
+# Four kinds of nucleotides are encoded as follows:
+#
+#   nucleotide | binary
+#   ---------- | ------
+#       A      |   00
+#       C      |   01
+#       G      |   10
+#     T / U    |   11
+#
+# NucleicAcids are filled from MSBs to LSBs and right-aligned so that all k-mers
+# are lexicographically ordered. For example, the memory layout of "TACG" is:
+#   64-bit: 0b 00 00 … 00 11 00 01 10
+#    4-mer:                T  A  C  G
+
 
 abstract type ShortSequence{N} <: BioSequence end
 
@@ -7,3 +38,4 @@ encoded_data(x::ShortSequence{16}) = reinterpret(UInt16, x)
 encoded_data(x::ShortSequence{8})  = reinterpret(UInt8, x)
 
 encoded_data_eltype(x::ShortSequence) = eltype(encoded_data(x))
+
