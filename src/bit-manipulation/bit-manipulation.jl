226a97955ca53b232fa69c4da5637672e5197b1d
Ben J. Ward <ward9250@gmail.com>
2018-10-01 12:39:15 +0100

    Refactor reversebits

--- src/bit-manipulation/bit-manipulation.jl
+++ src/bit-manipulation/bit-manipulation.jl
@@ -2,16 +2,16 @@
 include("bitindex.jl")
 
 @inline function reversebits(x::UInt64, ::BitsPerSymbol{2})
-     x = (x & 0x3333333333333333) <<  2 | (x & 0xCCCCCCCCCCCCCCCC) >>  2
+     x =  ((x & 0x3333333333333333) >>> 2) | ((x & 0x3333333333333333) <<<  2)
      x = reversebits(x, BitsPerSymbol{4}())
      return x
 end
 
 @inline function reversebits(x::UInt64, ::BitsPerSymbol{4})
-     x = (x & 0x0F0F0F0F0F0F0F0F) <<  4 | (x & 0xF0F0F0F0F0F0F0F0) >>  4
-     x = (x & 0x00FF00FF00FF00FF) <<  8 | (x & 0xFF00FF00FF00FF00) >>  8
-     x = (x & 0x0000FFFF0000FFFF) << 16 | (x & 0xFFFF0000FFFF0000) >> 16
-     x = (x & 0x00000000FFFFFFFF) << 32 | (x & 0xFFFFFFFF00000000) >> 32
+     x = ((x >>> 4) & 0x0F0F0F0F0F0F0F0F) | ((x & 0x0F0F0F0F0F0F0F0F) <<< 4)
+     x = ((x >>> 8) & 0x00FF00FF00FF00FF) | ((x & 0x00FF00FF00FF00FF) <<< 8)
+     x = ((x >>> 16) & 0x0000FFFF0000FFFF) | ((x & 0x0000FFFF0000FFFF) <<< 16)
+     x = ((x >>> 32) & 0x00000000FFFFFFFF) | ((x & 0x00000000FFFFFFFF) <<< 32)
      return x
 end
 
