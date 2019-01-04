771fface296a6824c877c0fb4bbf899c41fcf2d1
Ben J. Ward <ward9250@gmail.com>
2019-01-04 01:38:55 +0000

    Begin drafting a pairedseq datastore

--- src/datastores/pairedreads.jl
+++ src/datastores/pairedreads.jl
@@ -7,6 +7,23 @@ struct PairedSeqDatastore
     stream::IOStream
 end
 
+# PairedSeqDatastore Header
+# =========================
+# 
+# | Field             | Value  | Type   |
+# |:-----------------:|:------:|:------:|
+# | Magic number      | 0x0B56 | UInt16 |
+# | Datastore type    | 0x0001 | UInt16 |
+# | Version number    | 0x0001 | UInt16 |
+# | Maximum read size | N/A    | UInt64 |
+# | Chunk size        | N/A    | UInt64 |
+# | Number of Reads   | N/A    | UInt64 |
+
+const PSD_Version = 0x0001
+const PSD_Type    = 0x0001
+
+# Junk may get written in the unused space of the readpair chunk.
+
 function PairedSeqDatastore(rdrx, rdry, outname::String, minsize::UInt64, maxsize::UInt64)
     # Create and allocate the sequence and record objects.
     lread = FASTQ.Record()
@@ -16,12 +33,12 @@ function PairedSeqDatastore(rdrx, rdry, outname::String, minsize::UInt64, maxsiz
     
     chunksize::UInt64 = seq_data_len(DNAAlphabet{2}, maxsize)
     fd = open(outname, "w")
-    # Write magic no, version no, datastore type, read size, and chunk size.
-    sizepos = write(fd, 0x0B56, 0x0001, 0x0001, maxsize, chunksize)
+    # Write magic no, datastore type, version no, read size, and chunk size.
+    sizepos = write(fd, 0x0B56, PSD_Type, PSD_Version, maxsize, chunksize)
     # Write space for size variable (or number of read pairs).
     readpos = write(fd, UInt64(0))
     
-    @info string("readspos: ", readspos)
+    @info string("readspos: ", readpos)
     @info string("sizepos: ", sizepos)
     
     pairs = discarded = truncated = 0
@@ -60,12 +77,13 @@ function PairedSeqDatastore(rdrx, rdry, outname::String, minsize::UInt64, maxsiz
         copyto!(lseq, 1, lread, 1, ln)
         copyto!(rseq, 1, rread, 1, rn)
         
-        # Write the two sizes of the sequences one after the other.
-        write(fd, ln)
-        write(fd, rn)
+        # Write the two sizes reads one after the other: 
+        # For each sequence, write the size, and then the datachunk.
         
-        # Write the data chunk to file.
+        write(fd, ln)
         write(fd, lseq.data)
+        
+        write(fd, rn)
         write(fd, rseq.data)
     end
     
@@ -79,9 +97,35 @@ function PairedSeqDatastore(rdrx, rdry, outname::String, minsize::UInt64, maxsiz
     @info "Done writing paired sequences to datastore."
     @info string(discarded, " read pairs were discarded due to a too short sequence.")
     @info string(truncated, " reads were truncated to ", maxsize, " base pairs.")
-    @info string("Created paired sequence datastore with ", size, " sequence pairs.")
+    @info string("Created paired sequence datastore with ", pairs, " sequence pairs.")
     
     stream = open(outname, "r+")
     
     return PairedSeqDatastore(outname, maxsize, chunksize, readpos, nreads, stream)
+end
+
+function Base.open(::Type{PairedSeqDatastore}, filename::String)
+    fd = open(filename, "r")
+    magic = read(fd, UInt16)
+    dstype = read(fd, UInt16)
+    version = read(fd, UInt16)
+    
+    @assert magic == 0x0B56
+    @assert dstype == PSD_Type
+    @assert version == PSD_Version
+    
+    readsize = read(fd, UInt64)
+    chunksize = read(fd, UInt64)
+    nreads = read(fd, UInt64)
+    readpos_offset = position(fd)
+    
+    @info magic
+    @info dstype
+    @info version
+    @info readsize
+    @info chunksize
+    @info nreads
+    @info readpos_offset
+    
+    return PairedSeqDatastore(filename, readsize, chunksize, readpos_offset, nreads, fd)
 end
